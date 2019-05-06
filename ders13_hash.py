beff40aa28950f436208ef86a1f5ccf552c33497
Cemre Uzun <33479880+cemreuzun@users.noreply.github.com>
2019-05-06 18:55:03 +0300

    Update ders13_hash.py

--- ders13_hash.py
+++ ders13_hash.py
@@ -1,7 +1,8 @@
 # hash fonksiyonu
+# bu derste hash fonksiyonun mantığını sözel olarak işledik ve kod üzerinde sorular çözdük
 import random
 def createMyHashTable(N):
-    myTable=[]
+    myTable = []
     for i in range(N):
         myTable.append(0)
     return myTable
@@ -11,28 +12,28 @@ def my_hash(size,numberToBeInserted):
     return numberToBeInserted%size
 
 def insertMyHashTable(myTable,numberToBeInserted):
-    isCollision=False
-    index=my_hash(len(myTable),numberToBeInserted)
-    if(myTable[index]==0):
-        myTable[index]=1
+    isCollision = False
+    index = my_hash(len(myTable),numberToBeInserted)
+    if(myTable[index] == 0):
+        myTable[index] = 1
     else:
-        isCollision=True
+        isCollision = True
     return isCollision
 
 
-m_h_1=createMyHashTable(5)
+m_h_1 = createMyHashTable(5)
 print(insertMyHashTable(m_h_1,20))
 print(insertMyHashTable(m_h_1,20))
 
 
 
-def myTest(size=13,numberToBeInserted=10):
-    m_h_1=createMyHashTable(size)
-    c=0
+def myTest(size = 13,numberToBeInserted=10):
+    m_h_1 = createMyHashTable(size)
+    c = 0
     for s in range(numberToBeInserted):
-        number=random.randint(0,10000)
-        if(insertMyHashTable(m_h_1,number)==True):
-            c=c+1
+        number = random.randint(0,10000)
+        if(insertMyHashTable(m_h_1,number) == True):
+            c = c+1
     return c
 
-print(myTest(700,50),"*")
+print(myTest(81, 22),"**")
